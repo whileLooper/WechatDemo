986b1dbde1a76e2854118e482c3783b75abcbbf1
b004577 <bo.chen@macys.com>
2018-01-03 15:48:35 -0500

    forgot to remove --daemon

--- app/router.js
+++ app/router.js
@@ -6,5 +6,5 @@
 module.exports = app => {
   const { router, controller } = app;
   router.get('/', controller.home.index);
-  // router.get('/wechat', controller.wechat.index);
+  router.get('/wechat', controller.wechat.index);
 };
