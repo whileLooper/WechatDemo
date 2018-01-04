92484c75efeaadfd171d8b22dc47e75f5ace9991
b004577 <bo.chen@macys.com>
2018-01-03 21:38:05 -0500

    add return msg

--- app/controller/wechat.js
+++ app/controller/wechat.js
@@ -10,7 +10,14 @@ module.exports = app => {
   // 因为 Egg 需要用类的形式来组织，而 wechat 是通过 middleware 方法来生成中间件
   WechatController.prototype.index = wechat(config)
     .middleware(async (message, ctx) => {
-      return 'hello wechat';
+      return [
+        {
+          title: '你来我家接我吧',
+          description: '这是女神与高富帅之间的对话',
+          picurl: 'http://nodeapi.cloudfoundry.com/qrcode.jpg',
+          url: 'http://nodeapi.cloudfoundry.com/'
+        }
+      ];
     });
 
   return WechatController;
