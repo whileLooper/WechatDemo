0e400b6c62864343b9640c8c81b13a5b4c042b0f
b004577 <bo.chen@macys.com>
2018-01-05 14:53:40 -0500

    tsting

--- app/controller/wechat.js
+++ app/controller/wechat.js
@@ -4,11 +4,8 @@ const Controller = require('egg').Controller;
 const wechat = require('co-wechat');
 
 class WechatController extends Controller {
-  async index() {    
-    wechat(this.app.config.wechatConfig).middleware(async (message, ctx) => {
-      console.log(message);
-      ctx.body = message;
-    });
+  async index() { 
+    wechat(this.app.config.wechatConfig);
   }
 }
 
