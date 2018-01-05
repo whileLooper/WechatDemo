a0b9c136b00a12e70b08707a9e8a63f7af0fb8ed
b004577 <bo.chen@macys.com>
2018-01-04 21:11:06 -0500

    use app.all

--- app/router.js
+++ app/router.js
@@ -1,10 +1,10 @@
 'use strict';
-
 /**
  * @param {Egg.Application} app - egg application
  */
 module.exports = app => {
   const { router, controller } = app;
   router.get('/', controller.home.index);
-  router.get('/wechat', controller.wechat.index);
+  app.all('/wechat', controller.wechat.index);
+
 };
