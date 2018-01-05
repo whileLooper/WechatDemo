a7969a4810b349bf76b8bcc4285216a8ddcb8e64
b004577 <bo.chen@macys.com>
2018-01-05 13:05:09 -0500

    testing

--- app/controller/wechat.js
+++ app/controller/wechat.js
@@ -1,14 +1,15 @@
 'use strict';
 
+const Controller = require('egg').Controller;
 const wechat = require('co-wechat');
 
 module.exports = app => {
   class WechatController extends app.Controller {}
 
   // 因为 Egg 需要用类的形式来组织，而 wechat 是通过 middleware 方法来生成中间件
-  WechatController.prototype.index = this.ctx.app.use(wechat(app.config.wechatConfig).middleware( () => {
+  WechatController.prototype.index = wechat(app.config.wechatConfig).middleware( () => {
     app.service.wechatService.good();
-  }));
+  });
 
   return WechatController;
 };
