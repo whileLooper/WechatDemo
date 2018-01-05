142e33547a298956ae5a5d20b5b0482a91c437f3
b004577 <bo.chen@macys.com>
2018-01-04 22:18:46 -0500

    testing

--- app/controller/wechat.js
+++ app/controller/wechat.js
@@ -6,16 +6,7 @@ module.exports = app => {
   class WechatController extends app.Controller {}
 
   // 因为 Egg 需要用类的形式来组织，而 wechat 是通过 middleware 方法来生成中间件
-  WechatController.prototype.index = wechat({
-    port: '80',
-    token: 'bhu89ijnmko0',
-    appid: 'wxd61026a80b610e7c',
-    appSecret: '0e49baa28dd88439372c4788a619aa16',
-    encodingAESKey: 'HX2DzbFRh9qY4toNv9XVL1kYxgoYCTwoZ3haN92kAzl',
-    apiDomain: 'https://api.weixin.qq.com/',
-    accessTokenFilePath: './static/access_token.json'
-  }).middleware( () => {
-    // TODO
+  WechatController.prototype.index = wechat(app.config.wechatConfig).middleware( () => {
     app.service.wechatService.good();
   });
 
--- app/router.js
+++ app/router.js
@@ -4,6 +4,5 @@
  */
 module.exports = app => {
   const { router, controller } = app;
-  // router.get('/', controller.home.index);
   app.all('/wechat', controller.wechat.index);
 };
