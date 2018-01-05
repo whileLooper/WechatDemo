f977cce4ae7c34d159f7e0e917845ea6110382b5
b004577 <bo.chen@macys.com>
2018-01-04 20:59:14 -0500

    renew app secrect

--- config/config.default.js
+++ config/config.default.js
@@ -14,7 +14,7 @@ module.exports = appInfo => {
     port: '80',
     token: 'bhu89ijnmko0',
     appid: 'wxd61026a80b610e7c',
-    appSecret: '587a45fca820916e183acca9e41839e',
+    appSecret: '0e49baa28dd88439372c4788a619aa16',
     encodingAESKey: 'HX2DzbFRh9qY4toNv9XVL1kYxgoYCTwoZ3haN92kAzl',
     apiDomain: 'https://api.weixin.qq.com/',
     accessTokenFilePath: './static/access_token.json',
